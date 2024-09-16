2ff13ef1c385f8aa8b8ffbac20da53cc31bff046
Eshwarvr <rishvanjaseshwarv@gmail.com>
2024-09-16 11:07:58 +0530

    First Commit

---
+++ Project1.py
@@ -0,0 +1,14 @@
+for i in enumerate([1, 2, 3, 4]):
+    print(i)
+i = 0
+while i < 50:
+    print(i)
+    i += 1
+    break
+print('done')
+
+a1 = ['a', 'b', 'c', 'b', 'd', 'c']
+for i in range(len(a1)):
+    for j in range(i + 1, len(a1)):
+        if a1[i] == a1[j]:
+            print(a1[i])
